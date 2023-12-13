dc131a15d024d18910c3e2c53468e3e54f687dc9
simon <simon.hamner@gmail.com>
2023-12-13 18:14:33 +0100

    added StenSaxPase fragment and StenSaxPase ViewModel

---
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/StenSaxPaseViewModel.kt
@@ -0,0 +1,7 @@
+package com.hfad.klientutvecklingsprojekt
+
+import androidx.lifecycle.ViewModel
+
+class StenSaxPaseViewModel : ViewModel() {
+    //add bussiness logic for sten sax pase mini-game
+}

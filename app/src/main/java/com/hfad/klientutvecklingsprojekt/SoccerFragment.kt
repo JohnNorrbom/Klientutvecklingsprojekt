bab9a7e03d31bb22feec0167ee2af3f12c5ca758
Ferdinand Öhrn <ohrnferdinand@gmail.com>
2023-12-18 12:06:31 +0100

    added animations to soccer minigame

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/SoccerFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/SoccerFragment.kt
@@ -19,20 +19,38 @@ class SoccerFragment : Fragment() {
     ): View? {
         _binding = FragmentSoccerBinding.inflate(inflater,container,false)
         val view = binding.root
-        //binding shooters
+        //set all shooters invisible except for white
         binding.yellowShooter.visibility = View.INVISIBLE
         binding.redShooter.visibility = View.INVISIBLE
         binding.greenShooter.visibility = View.INVISIBLE
         binding.blueShooter.visibility = View.INVISIBLE
+
+        //set all goalies invisible except for white
+        binding.blueGoalieRight.visibility = View.INVISIBLE
+
+        //set all ball animations invisible except right in animation
+
+        binding.ballLeft.visibility = View.INVISIBLE
+        binding.ballRight.visibility = View.INVISIBLE
+        binding.ballMid.visibility = View.INVISIBLE
+        binding.ballLeftIn.visibility = View.INVISIBLE
+        binding.ballMidIn.visibility = View.INVISIBLE
+
         //create animation for shooter
         var animationDrawable = binding.whiteShooter.background as AnimationDrawable
-        var ballAnimation = binding.ballRight.background as AnimationDrawable
+        var ballAnimation = binding.ballRightIn.background as AnimationDrawable
+        var goalieAnimation = binding.blueGoalieLeft.background as AnimationDrawable
         animationDrawable.apply {
             start()
         }
         ballAnimation.apply {
             start()
         }
+
+        goalieAnimation.apply{
+            start()
+        }
+
         return view
     }
     override fun onDestroyView() {

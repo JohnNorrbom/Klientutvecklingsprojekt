3cf8a06764ee34bc511ddaa686401edd06164748
pont <lindholmpontus@outlook.com>
2023-12-28 15:12:39 +0100

    lite dice kod

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/DiceFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/DiceFragment.kt
@@ -5,19 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.hfad.klientutvecklingsprojekt.databinding.FragmentDiceBinding
 import com.hfad.klientutvecklingsprojekt.databinding.FragmentPlayerInfoBinding
+import kotlin.random.Random
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DiceFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DiceFragment : Fragment() {
     private var _binding: FragmentDiceBinding? = null
     private val binding get()  = _binding!!
@@ -31,24 +28,11 @@ class DiceFragment : Fragment() {
         // Inflate the layout for this fragment
         return view
     }
+    fun rollDice(imageView: ImageView) {
+        val randomNumber = Random.nextInt(6) + 1
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Dice.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DiceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val drawableResource = resources.getIdentifier("dice$randomNumber", "drawable", packageName)
+        imageView.setImageResource(drawableResource)
     }
+
 }

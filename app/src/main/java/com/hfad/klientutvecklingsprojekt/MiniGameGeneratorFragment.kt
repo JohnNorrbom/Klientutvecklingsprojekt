0024ff3e6134d33bd7019eb335ba5af0d6d58664
John <john.norrbom@hotmail.com>
2023-12-28 15:13:28 +0100

    minigamegenereator

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/MiniGameGeneratorFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/MiniGameGeneratorFragment.kt
@@ -5,17 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.hfad.klientutvecklingsprojekt.databinding.FragmentDiceBinding
-import com.hfad.klientutvecklingsprojekt.databinding.FragmentPlayerInfoBinding
+import com.hfad.klientutvecklingsprojekt.databinding.FragmentMiniGameGeneratorBinding
 
-class DiceFragment : Fragment() {
-    private var _binding: FragmentDiceBinding? = null
+class MiniGameGeneratorFragment : Fragment() {
+    private var _binding: FragmentMiniGameGeneratorBinding? = null
     private val binding get()  = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentDiceBinding.inflate(inflater,container,false)
+        _binding = FragmentMiniGameGeneratorBinding.inflate(inflater,container,false)
         val view = binding.root
         // Inflate the layout for this fragment
         return view

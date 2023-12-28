fa515303570253f7f554ad4bf337f0b3de9e713c
W1LST3R <williampet02@gmail.com>
2023-12-28 18:04:50 +0100

    uppdate av lobby

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/DiceFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/DiceFragment.kt
@@ -6,18 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hfad.klientutvecklingsprojekt.databinding.FragmentDiceBinding
-import com.hfad.klientutvecklingsprojekt.databinding.FragmentPlayerInfoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DiceFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DiceFragment : Fragment() {
     private var _binding: FragmentDiceBinding? = null
     private val binding get()  = _binding!!
@@ -32,23 +22,4 @@ class DiceFragment : Fragment() {
         return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Dice.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DiceFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
-}

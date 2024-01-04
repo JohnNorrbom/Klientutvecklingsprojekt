c8581197b66a38ddeec20adbfbf146772eb9bfb1
simon <simon.hamner@gmail.com>
2024-01-04 14:33:40 +0100

    stensaxpase

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/stensaxpase/StenSaxPaseData.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/stensaxpase/StenSaxPaseData.kt
@@ -1,21 +1,45 @@
 package com.hfad.klientutvecklingsprojekt.stensaxpase
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.database
 import com.hfad.klientutvecklingsprojekt.gamestart.GameModel
 
 object StenSaxPaseData {
-    private var _gameModel : MutableLiveData<GameModel> = MutableLiveData()
-    var gameModel : LiveData<GameModel> = _gameModel
+    private var _gameModel : MutableLiveData<GameModel?> = MutableLiveData()
+    var gameModel : MutableLiveData<GameModel?> = _gameModel
     val database = Firebase.database("https://klientutvecklingsprojekt-default-rtdb.europe-west1.firebasedatabase.app/")
-    val myRef = database.getReference("Space Party")
+    val myRef = database.getReference("Game Data")
 
     fun saveGameModel(model: GameModel){
+        Log.d("GameModel","${model}")
         _gameModel.postValue(model)
-        if(model.gameID!= "-1"){
-            model.gameID?.let { myRef.child(it).setValue(model) }
+        myRef.child(model.gameID?:"").setValue(model)
+
+    }
+
+    fun fetchGameModel(){
+        gameModel.value?.apply {
+            myRef.addValueEventListener(gameListener)
         }
+    }
+
+    val gameListener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            val id = gameModel.value?.gameID
+            val model = snapshot.child(id ?: "").getValue(GameModel::class.java)
+            _gameModel.postValue(model)
+
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            TODO("Not yet implemented")
+        }
+
     }
 }

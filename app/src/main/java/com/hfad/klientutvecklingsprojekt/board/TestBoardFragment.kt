407cda039aedc11e0ba9148eada6d66ad75f769e
simon <simon.hamner@gmail.com>
2024-01-06 23:26:55 +0100

    testboardFragment, miniGame fix stenSaxPaseWait, fix

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/board/TestBoardFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/board/TestBoardFragment.kt
@@ -254,7 +254,8 @@ class TestBoardFragment : Fragment() {
         //  DICE BUTTON LISTENER
         dice?.setOnClickListener {
             //soundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f)
-            var randomInt = Random.nextInt(6) + 1
+            var randomInt = 5
+                //Random.nextInt(6) + 1
             var destination = "dice" + randomInt
             val resourceId = resources.getIdentifier(
                 destination,
@@ -279,7 +280,8 @@ class TestBoardFragment : Fragment() {
             if (currentImageViewIndex % 20 == 5 || currentImageViewIndex % 20 == 10 || currentImageViewIndex % 20 == 19) {
                 //minigame
                 //  Pick random game
-                localRandomVal = Random.nextInt(4)
+                localRandomVal = 0
+                    //Random.nextInt(4)
                 //laddauppminigamesiffra,
                 //gör en listener som kallar på setMinigame
                 // currentPlayer startar minigame

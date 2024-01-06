51946d5b224d84c9c1c5d8c9eae7a097552855bf
John <32957863+JohnNorrbom@users.noreply.github.com>
2024-01-06 13:40:12 +0100

    leaderboard

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/leaderboard/leaderBoardFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/leaderboard/leaderBoardFragment.kt
@@ -6,13 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.hfad.klientutvecklingsprojekt.R
+import com.hfad.klientutvecklingsprojekt.databinding.FragmentLeaderBoardBinding
+import com.hfad.klientutvecklingsprojekt.databinding.FragmentLobbyBinding
 
 class leaderBoardFragment : Fragment() {
+    private var _binding: FragmentLeaderBoardBinding? = null
+    private val binding get()  = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leader_board, container, false)
+        _binding = FragmentLeaderBoardBinding.inflate(inflater,container,false)
+        val view = binding.root
+        binding.root.rootView.setBackgroundResource(R.drawable.leaderboard)
+        return view
     }
 }

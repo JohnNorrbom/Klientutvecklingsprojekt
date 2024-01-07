3bc37ad728940954b6f07d19a609ee60fd6cd8cd
pont <lindholmpontus@outlook.com>
2024-01-07 21:11:27 +0100

    deploy....

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/board/TestBoardFragment.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/board/TestBoardFragment.kt
@@ -321,7 +321,6 @@ class TestBoardFragment : Fragment() {
         dice?.setOnClickListener {
             //soundPool.play(soundId, 1.0f, 1.0f, 1, 0, 1.0f)
             var randomInt = Random.nextInt(6) + 1
-//            var randomInt = 5
             var destination = "dice" + randomInt
             var resourceId = resources.getIdentifier(
                 destination,
@@ -346,8 +345,8 @@ class TestBoardFragment : Fragment() {
             if (currentImageViewIndex % 20 == 5 || currentImageViewIndex % 20 == 10 || currentImageViewIndex % 20 == 19) {
                 //minigame
                 //  Pick random game
-                localRandomVal = Random.nextInt(2) + 1
-//                localRandomVal = 2
+                //localRandomVal = Random.nextInt(2) + 1
+                localRandomVal = 0
                 //laddauppminigamesiffra,
                 //gör en listener som kallar på setMinigame
                 // currentPlayer startar minigame

dec57c21a417828a2d8f0f9503c2195ab76edeb6
John <32957863+JohnNorrbom@users.noreply.github.com>
2024-01-08 03:37:04 +0100

    Kdoc

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/MainActivity.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/MainActivity.kt
@@ -11,12 +11,19 @@ import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.hfad.klientutvecklingsprojekt.databinding.ActivityMainBinding
 
-
+/**
+ *
+ * @author John
+ *
+ * MainActivity:
+ *
+ * Gör headern osynlig
+ *
+ */
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding // Ensure your binding variable is declared
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //  Makes the status and navigation bar of the phone transparent
         if (Build.VERSION.SDK_INT>= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS); getWindow().getDecorView()
                 .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);

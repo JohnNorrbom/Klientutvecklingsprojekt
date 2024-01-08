49f91b6dc8c692e98426176c1f79da2e0f559dc7
John <32957863+JohnNorrbom@users.noreply.github.com>
2024-01-08 03:17:07 +0100

    Kdoc

--- app/src/main/java/com/hfad/klientutvecklingsprojekt/board/BoardModel.kt
+++ app/src/main/java/com/hfad/klientutvecklingsprojekt/board/BoardModel.kt
@@ -1,3 +1,12 @@
 package com.hfad.klientutvecklingsprojekt.board
 
+/**
+ *
+ * Tabell med:
+ * gameID som håller koll så alla spelare är i samma lobby/game
+ * currentPlayerID som håller koll på vems tur det är
+ * playerCount som håller koll på hur många spelare det är med i lobbyn/gamet
+ * randomVal som håller koll på vilket minigame som ska startas
+ *
+ */
 data class BoardModel (val gameID: String?, val currentPlayerID: String?, val playerCount: Int?, val randomVal: Int?)
